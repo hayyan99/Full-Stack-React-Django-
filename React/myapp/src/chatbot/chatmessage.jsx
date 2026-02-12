db0bd03e4241a7b5e9bb3d754b322e3f8c69acea
hayyan99 <hayyanzuberi@hotmail.co.uk>
2026-02-12 19:52:45 +0500

    #

--- React/myapp/src/chatbot/chatmessage.jsx
+++
@@ -1,7 +0,0 @@
-// export default function ChatMessage({ message }) {
-//   return (
-//     <div className={`message ${message.sender}`}>
-//       <div className="bubble">{message.text}</div>
-//     </div>
-//   );
-// }
